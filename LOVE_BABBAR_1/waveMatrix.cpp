36a88f97b1899708829c76dc744be0130b75877a
KISHLAY568 <kishlayk568@gmail.com>
2024-07-31 12:46:25 +0530

    first commit

---
+++ LOVE_BABBAR_1/waveMatrix.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+
+
+using namespace std;
+void wavePrintMatrix(vector<vector<int>>v)
+{
+    int r = v.size();
+    int c = v[0].size();
+    for(int i=0;i<c;i++)
+    {
+        if((i & 1)==0)
+        {
+            for(int j=0;j<r;j++)
+            {
+                cout<<v[j][i]<<" ";
+            }
+
+        }
+        else{
+            for(int j=r-1;j>=0;j--)
+            {
+                cout<<v[j][i]<<" ";
+            }
+        }
+    }
+}
+int main()
+{
+    vector<vector<int>> a{{1,2,3,4},
+    {5,6,7,8},
+    {9,10,11,12}};
+
+    wavePrintMatrix(a);
+    return 0;
+}
