e1a6f3f0793e20279c97005e1d8d9519802145b1
KISHLAY568 <kishlayk568@gmail.com>
2024-11-05 22:30:31 +0530

    cpp

---
+++ rohitNegi/heapSort.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+void heapify(int arr[], int index, int size)
+{
+    int largest = index;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+
+    if (left < size && arr[left] > arr[largest])
+    {
+        largest = left;
+    }
+    if (right < size && arr[right] > arr[largest])
+    {
+        largest = right;
+    }
+
+    if (largest != index)
+    {
+        swap(arr[largest], arr[index]);
+        heapify(arr, largest, size);
+    }
+}
+
+void buildMaxHeap(int arr[], int size)
+{
+    for (int i = size / 2 - 1; i >= 0; i--)
+    {
+        heapify(arr, i, size);
+    }
+}
+
+void sort(int arr[], int size)
+{
+    for (int i = size - 1; i >= 0; i--)
+    {
+        swap(arr[i], arr[0]);
+        heapify(arr, 0, i);
+    }
+}
+
+void print(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int arr[] = {1,
+                 3,
+                 5,
+                 4,
+                 6,
+                 13,
+                 10,
+                 9,
+                 8,
+                 15,
+                 17};
+
+    buildMaxHeap(arr, 11);
+    print(arr, 11);
+    sort(arr, 11);
+    cout << endl;
+    print(arr, 11);
+}
