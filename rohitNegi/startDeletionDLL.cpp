89297e41238d52501c35858583bee5b1f1af7752
KISHLAY568 <kishlayk568@gmail.com>
2025-01-01 20:01:17 +0530

    cpp

---
+++ rohitNegi/startDeletionDLL.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+int main()
+{
+    int arr[] = {2, 4, 6, 8, 9};
+    Node *head = NULL, *tail = NULL;
+    for (int i = 0; i < 5; i++)
+    {
+        if (head == NULL)
+        {
+            head = new Node(arr[i]);
+            tail = head;
+        }
+        else
+        {
+            Node *temp = new Node(arr[i]);
+            tail->next = temp;
+            temp->prev = tail;
+            tail = temp;
+        }
+    }
+
+    if (head != NULL)
+    {
+        if (head->next == NULL)
+        {
+            delete head;
+            head = NULL;
+        }
+        else
+        {
+            Node *temp = head;
+            head = head->next;
+            delete temp;
+            head->prev = NULL;
+        }
+    }
+
+    Node *trav = head;
+    while (trav)
+    {
+        cout << trav->data << " ";
+        trav = trav->next;
+    }
+
+    return 0;
+}
