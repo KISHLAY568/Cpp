60749036115560983a7af0e349950e68464b84b6
KISHLAY568 <kishlayk568@gmail.com>
2025-03-07 21:36:30 +0530

    cpp

---
+++ rohitNegi/minElementArray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+int main()
+{
+    int size;
+    cin >> size;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int min = INT_MAX;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+
+    cout << min;
+    return 0;
+}
